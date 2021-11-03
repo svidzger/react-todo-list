1f70b1219e1ceaa97a51e43d59575c587817f96d
svidzger <german.svidzinskij@gmail.com>
2021-11-04 01:46:07 +0200

    Added date picker

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,25 @@
-import React, { useState, useRef } from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Tooltip from '@mui/material/Tooltip';
-import TextField from '@mui/material/TextField';
+import React, { useState, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Tooltip from "@mui/material/Tooltip";
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+import { format } from "date-fns";
+import { fi } from 'date-fns/locale';
 
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
 
 function TodoList() {
-
   const [todoList, setTodoList] = useState([]);
   const [todo, setTodo] = useState({
-    date: '',
-    desc: '',
-    priority: ''
+    date: null,
+    desc: "",
+    priority: "",
   });
 
   const gridRef = useRef();
@@ -23,37 +27,44 @@ function TodoList() {
   const addTodo = () => {
     setTodoList([todo, ...todoList]);
     setTodo({
-      date: '',
-      desc: '',
-      priority: ''
+      date: null,
+      desc: "",
+      priority: "",
     });
-  }
+  };
 
   const deleteTodo = () => {
     if (gridRef.current.getSelectedNodes().length > 0) {
-      setTodoList(todoList.filter((todo, index) => index !== gridRef.current.getSelectedNodes()[0].childIndex));
+      setTodoList(
+        todoList.filter(
+          (todo, index) =>
+            index !== gridRef.current.getSelectedNodes()[0].childIndex
+        )
+      );
     } else {
-      alert('Empty field');
+      alert("Empty field");
     }
-
-  }
+  };
 
   const columns = [
-    { field: 'date', sortable: true, filter: true, floatingFilter: true },
-    { field: 'desc', sortable: true, filter: true, floatingFilter: true },
+    { field: "date", sortable: true, filter: true, floatingFilter: true },
+    { field: "desc", sortable: true, filter: true, floatingFilter: true },
     {
-      field: 'priority', sortable: true, filter: true, floatingFilter: true,
+      field: "priority",
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
       cellClassRules: {
-        'priority-high': params => params.value === 'High',
-        'priority-medium': params => params.value === 'Medium',
-        'priority-low': params => params.value === 'Low',
+        "priority-high": (params) => params.value === "High",
+        "priority-medium": (params) => params.value === "Medium",
+        "priority-low": (params) => params.value === "Low",
       },
-    }
+    },
   ];
 
   const inputChanged = (e) => {
-    setTodo({ ...todo, [e.target.name]: e.target.value })
-  }
+    setTodo({ ...todo, [e.target.name]: e.target.value });
+  };
 
   return (
     <div>
@@ -61,57 +72,62 @@ function TodoList() {
         direction="row"
         spacing={2}
         justifyContent="center"
-        style={{ marginTop: 20 }}>
-        <TextField
-          label="Date"
-          name="date"
-          value={todo.date}
-          onChange={inputChanged}
-          size="small"
+        style={{ marginTop: 20 }}
+      >
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DatePicker
+            label="Date"
+            locale={fi}
+            value={todo.date}
+            onChange={(date) => {
+              const formattedDate = format(date, "dd MMM, yyyy");
+              setTodo({ ...todo, date: formattedDate });
+            }}
+            renderInput={(params) => <TextField {...params} />}
           />
+        </LocalizationProvider>
         <TextField
           label="Descriptions"
           name="desc"
           value={todo.desc}
           onChange={inputChanged}
-          size="small"
-          />
+        />
         <TextField
           label="Priority"
           name="priority"
           value={todo.priority}
           onChange={inputChanged}
-          size="small"
-          />
+        />
 
-        <Button variant="outlined"
-          size="small"
-          onClick={addTodo}>
+        <Button variant="outlined" size="small" onClick={addTodo}>
           Add
         </Button>
         <Tooltip title="Choose ToDo to delete">
-        <Button endIcon={<DeleteIcon/>}
-          variant="outlined"
-          color="error"
-          size="small"
-          onClick={deleteTodo}>
-          Delete
-        </Button>
+          <Button
+            endIcon={<DeleteIcon />}
+            variant="outlined"
+            color="error"
+            size="small"
+            onClick={deleteTodo}
+          >
+            Delete
+          </Button>
         </Tooltip>
       </Stack>
 
-      <div className="ag-theme-material" style={{ height: 400, width: 600, margin: 'auto' }}>
+      <div
+        className="ag-theme-material"
+        style={{ height: 400, width: 600, margin: "auto" }}
+      >
         <AgGridReact
           ref={gridRef}
-          onGridReady={params => gridRef.current = params.api}
+          onGridReady={(params) => (gridRef.current = params.api)}
           rowSelection="single"
           animateRows={true}
           rowData={todoList}
           columnDefs={columns}
         />
       </div>
-
-
     </div>
   );
 }
--- src/components/TodoTable.js
+++ src/components/TodoTable.js
@@ -4,15 +4,16 @@ function TodoTable(props) {
   return (
     <table>
       <tbody>
-        {
-          props.todoList.map((todo, index) =>
-            <tr key={index}>
-              <td>{todo.date}</td>
-              <td>{todo.desc}</td>
-              <td>{todo.priority}</td>
-              <td><button onClick={() => props.deleteTodo(index)}>Delete</button></td>
-            </tr>)
-        }
+        {props.todoList.map((todo, index) => (
+          <tr key={index}>
+            <td>{todo.date}</td>
+            <td>{todo.desc}</td>
+            <td>{todo.priority}</td>
+            <td>
+              <button onClick={() => props.deleteTodo(index)}>Delete</button>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
